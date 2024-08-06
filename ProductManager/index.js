6ed715e65eb42ff95b76547b9cd8acb4c633bd22
Delson Deantonio Cruz <55507159+DelsonCruz@users.noreply.github.com>
2024-08-06 12:08:09 -0500

    Add files via upload

--- ProductManager/index.js
+++ ProductManager/index.js
@@ -1,52 +1,40 @@
-// import express from 'express'
+import express from 'express'
 import upload from './config/multer.js'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import cors from 'cors'
-
 import messageModel from './models/messages.js'
 import indexRouter from './routes/indexRouter.js'
 import cookieParser from 'cookie-parser'
 import loginRouter from './routes/loginRouter.js'
-import config from './config/config.js'
-import productsRouter from './routes/productsRouter.js'
-import serverRouter from './server/serverRouter.js'
-
-
 import { Server } from 'socket.io'
 import { engine } from 'express-handlebars'
 import { __dirname } from './path.js'
-import { fork } from 'child_process'
 // import { configDotenv } from 'dotenv'
 // import { MongoTailableCursorError } from 'mongodb'
-
-
-
-
+import config from './config.js'
+import dotenv from 'dotenv'
+import { fork } from 'child_process'
 
 
 //Configuraciones o declaraciones
-// const app = express();
-// const PORT = 8000;
+const app = express();
+const PORT = 8000;
 
 //Server
-// const server = app.listen(PORT, () => {
-//     console.log(`Server on port ${PORT}`)
-// })
+const server = app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
 
-// loginRouter.listen(PORT, () => {
-//     console.log(`Login on port ${PORT}`)
-// });
+loginRouter.listen(PORT, () => {
+    console.log(`Login on port ${PORT}`)
+});
 
-
-
-// const io = new Server(server)
+const io = new Server(server)
 
 // Conexion con DataBase
-// mongoose.connect("mongodb+srv://delsong91:<password>@cluster0.covmyfh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://delsong91:<password>@cluster0.covmyfh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-// .then(() => console.log("DB esta online"))
-// .catch(e => console.log(e))
+.then(() => console.log("DB esta online"))
+.catch(e => console.log(e))
 
 //Middlewares
 app.use(express.json())
@@ -55,22 +43,22 @@ app.use('/', indexRouter);
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/api/products', productsRouter)
+
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', __dirname + '/views')
 app.use(cookieParser(process.env.COOKIES_SECRET))
 
 
-// app.use(session({
-//     secret: "coderSecret",
-//     resave: true,
-//     store: MongoStore.create({
-//         mongoUrl: "mongodb+srv://delsong91:<password>@cluster0.covmyfh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-//         ttl: 60 * 60
-//     }),
-//     saveUnitialized: true
-// }))
+app.use(session({
+    secret: "coderSecret",
+    resave: true,
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://delsong91:<password>@cluster0.covmyfh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        ttl: 60 * 60
+    }),
+    saveUnitialized: true
+}))
 
 
 
@@ -80,15 +68,15 @@ app.use(cookieParser(process.env.COOKIES_SECRET))
 dotenv.config();
 
 
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: true,
-//     store: MongoStore.create({
-//         mongoUrl: process.env.MONGO_DB_URL,
-//         ttl: 60 * 60
-//     }),
-//     saveUnitialized: true
-// }))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_DB_URL,
+        ttl: 60 * 60
+    }),
+    saveUnitialized: true
+}))
 
 app.get('/suma', (req, res) => {
 
@@ -151,25 +139,25 @@ app.get('/home', (req, res) => {
 
 
 
-// io.on('connection', (socket) => {
-//     console.log("Conexion con Socket.io")
+io.on('connection', (socket) => {
+    console.log("Conexion con Socket.io")
 
   
-//     socket.on('mensaje', async (mensaje) => {
+    socket.on('mensaje', async (mensaje) => {
   
-//         try{
-//             await messageModel.create(mensaje)
-//             const mensajes = await messageModel.find()
-//             io.emit('mensajeLogs', mensajes)
-//         }catch(e){
-//             io.emit('mensajeLogs', e)
-//             //aca se envia cualquier mensaje, teniendo en cuenta que
-//             // se debe enviar asi exista un error
-//         }
+        try{
+            await messageModel.create(mensaje)
+            const mensajes = await messageModel.find()
+            io.emit('mensajeLogs', mensajes)
+        }catch(e){
+            io.emit('mensajeLogs', e)
+            //aca se envia cualquier mensaje, teniendo en cuenta que
+            // se debe enviar asi exista un error
+        }
         
-//     })
+    })
 
-// })
+})
 
 
 
@@ -185,6 +173,46 @@ app.post('/upload', upload.single('product'), (req, res) => {
 })
 
 
+// Mailing
+
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    // el puerto de servicio 485 tambien se puede 
+    // usar(pero usualmente se bloquea para usuarios estandar)
+    port: 587,
+    auth:{
+        user: "nak.rock2@gmail.com",
+        pass: "fpog vbtm xcsx abtl"
+    }
+
+});
+
+
+app.get ('/mail', async (req, res) => {
+
+        const mail = await transport.sendMail({
+            from: 'Test coder<nak.rock2@gmail.com>',
+            to: "nak.rock2@gmail.com",
+            subject: "Correo test",
+            html: `
+                <div>
+                <h1>
+                holA
+                </h1>
+                </div>
+                    `,
+            attachments:[]
+            // para adjuntar archivos debe subirlo al repository (recomendado)
+            // attachments:[{
+            // filename: 'imagen.jpg(archivo.pdf)',
+            // path: _dirname+ '/ruta/imagen.jpg',
+            // ID(alias) que debe tener el archivo
+            // cid: 'imagen'
+            // }]
+        })
+        console.log(mail)
+        res.status(200).send("Mail enviado")
+}) 
 
 
 
