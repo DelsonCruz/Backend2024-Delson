f9b8d4840641c0f09096182ad6249cc1d40d9e18
Delson Deantonio Cruz <55507159+DelsonCruz@users.noreply.github.com>
2024-08-16 16:17:52 -0500

    Update chatRouter.js

--- ProductManager/routes/chatRouter.js
+++ ProductManager/routes/chatRouter.js
@@ -5,9 +5,6 @@ const chatRouter = Router()
 chatRouter.get('/', (req, res) => {
 
     
-    
-    // no se consulta el array en la ruta "templates/chat", {}
-    // res.render("templates/chat", {})
 })
 
 export default chatRouter
