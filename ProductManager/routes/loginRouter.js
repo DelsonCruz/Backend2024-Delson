f5c2a32d894889712ac21acdb64b23f21ea0ea19
Delson Deantonio Cruz <55507159+DelsonCruz@users.noreply.github.com>
2024-08-16 16:18:34 -0500

    Update loginRouter.js

--- ProductManager/routes/loginRouter.js
+++ ProductManager/routes/loginRouter.js
@@ -8,14 +8,14 @@ const PORT = 8001;
 loginRouter.engine('handlebars', exphbs());
 loginRouter.set('view engine', 'handlebars');
 
-// Ruta para mostrar el formulario de login
+
 loginRouter.get('/login', (req, res) => {
     res.render('login');
 });
 
-// Ruta para manejar el envío del formulario de login
+
 loginRouter.post('/login', (req, res) => {
-    // Aquí maneja la lógica de autenticación (ver punto 3)
+    
 });
 
 
