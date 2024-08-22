e24f669a8479b92d6f8f393f3e6cac671f3c47ab
Delson Deantonio Cruz <55507159+DelsonCruz@users.noreply.github.com>
2024-08-22 14:01:10 -0500

    Update config.js

--- ProductManager/config/config.js
+++ ProductManager/config/config.js
@@ -1,18 +1,14 @@
 import dotenv from 'dotenv'
-// import { password } from 'pg/lib/defaults';
 
 // environment = entorno
 // en el entorno = 'DEVELOPMENT' usa la ruta PORT = 6000
 // en el entorno = 'PRODUCTION' usa la ruta PORT = 8000
 const environment = 'PRODUCTION';
 
-// aca consulto la configuracion de las variables de entorno
 dotenv.config({
     path: environment == 'PRODUCTION' ? './.env.production': './.env.development'
 
 })
-
-// y se exporta como un objeto de configuracion
 
 const config = {
 
